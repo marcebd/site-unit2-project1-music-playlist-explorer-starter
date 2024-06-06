c827918c1253a381a8376cb2eb1bfd72b6162e3e
Marcela Billingslea Durini <marcebd@meta.com>
2024-06-06 14:56:21 -0700

    version 4

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,28 @@
+/* Open on click */
+//Use if statements to see what was clicked
 
+//Call the populate div function
+
+/* Populate song div */
+
+/* Add more song divs */
+
+/* Shuffle songs */
+
+//  .appendChild
+// Create Element
+
+/******************  HOME *******************/
+function home() {
+
+}
+/******************* MODAL *******************/
+function modal() {
+
+}
+
+/******************* MAIN ********************/
+function main() {
+
+}
+data.playlists[0].songs[0].album
