22fbf9ac30e28a4edbf7516f059e26a213605bee
Marcela Billingslea Durini <marcebd@meta.com>
2024-06-06 19:56:43 -0700

    Javascript started

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,5 +1,4 @@
 /* Open on click */
-//Use if statements to see what was clicked
 
 //Call the populate div function
 
@@ -11,10 +10,46 @@
 
 //  .appendChild
 // Create Element
+function playlistName(playListTitleDiv, name) {
+   playListTitleDiv.innerText = $(name).text();
+}
+
+function playListCoverImg(playListCoverImg, img) {
+   playListCoverImg.setAttribute("src", img);
+}
+
+function playListCreator(playListCreator, creator) {
+   playListCreator.innerText = $(creator).text();
+}
+
+function likeCount(likeCount, likeCountNum) {
+   likeCount.innerText = $(likeCountNum).text();
+}
 
 /******************  HOME *******************/
 function home() {
+   // Create a container to hold the dynamically generated divs
+  const playlistContainer = document.createElement("div");
+   for (const playlist in data.playlists) {
+      //Populate playlist cover img
+      var playListCoverImg = document.getElementById("playlistCoverImg");
+      var img = data.playlists[playlist].playlist_art;
+      playListCoverImg(playListCoverImg, img);
 
+      //Populate playlist name
+      var playListName = document.getElementById("playName");
+      var name = data.playlists[playlist].playlist_name;
+      playListName(playListName, name);
+
+      //Populate playlist creator
+      var playListCreator = document.getElementById("creatorsName");
+      var creator = data.playlists[playlist].playlist_creator;
+      playListCreator(playListCreator, creator);
+
+      //Populate like-count
+      var likeCount = document.getElementById("likeCount");
+      var likeCountNum = data.playlists[playlist].likeCount;
+   }
 }
 /******************* MODAL *******************/
 function modal() {
