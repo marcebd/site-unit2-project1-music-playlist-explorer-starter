e55d77ac4e50e2e52024125bf0d81cd1106d3a05
Marcela Billingslea Durini <marcebd@meta.com>
2024-06-06 21:44:47 -0700

    home improved

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -10,12 +10,14 @@
 
 //  .appendChild
 // Create Element
+
+/******************  HOME *******************/
 function playlistName(playListTitleDiv, name) {
    playListTitleDiv.innerText = $(name).text();
 }
 
-function playListCoverImg(playListCoverImg, img) {
-   playListCoverImg.setAttribute("src", img);
+function playListImg(playListImg, img) {
+   playListImg.setAttribute("src", img);
 }
 
 function playListCreator(playListCreator, creator) {
@@ -26,7 +28,27 @@ function likeCount(likeCount, likeCountNum) {
    likeCount.innerText = $(likeCountNum).text();
 }
 
-/******************  HOME *******************/
+function getCoverImage(playlist){
+   var img = data.playlists[playlist].playlist_art;
+   return img;
+}
+
+function getPlaylistName(playlist){
+   var name = data.playlists[playlist].playlist_name;
+   return name;
+}
+
+function getplaylistCreator(playlist){
+   var creator = data.playlists[playlist].playlist_creator;
+   return creator;
+}
+
+function getLikeCount(playlist){
+   var likeCountNum = data.playlists[playlist].likeCount;
+   return likeCountNum;
+}
+
+
 function home() {
    // Create a container to hold the dynamically generated divs
    const playlistContainer = document.createElement("div");
@@ -39,12 +61,65 @@ function home() {
 
       //Populate playlist cover img
       var playListCoverImg = document.getElementById("playlistCoverImg");
-      var img = data.playlists[playlist].playlist_art;
-      playListCoverImg(playListCoverImg, img);
+      var img = getCoverImage(playlist);
+      playListImg(playListCoverImg, img);
 
       //Populate playlist name
       var playListName = document.getElementById("playName");
-      var name = data.playlists[playlist].playlist_name;
+      var name = getPlaylistName(playlist);
+      playListName(playListName, name);
+
+      //Populate playlist creator
+      var playListCreator = document.getElementById("creatorsName");
+      var creator = getPlaylistCreator(playlist);
+      playListCreator(playListCreator, creator);
+
+      //Populate like-count
+      var likeCount = document.getElementById("likeCount");
+      var likeCountNum = getLikeCount(playlist);
+      likeCount(likeCount, likeCountNum);
+      //Append Child
+      playlistContainer.appendChild(playlistDiv);
+   }
+   // Append the container to the body
+   const targetParent = document.getElementById("playlistCards");
+   targetParent.appendChild(playlistContainer);
+}
+/******************* MODAL *******************/
+function title(titleDiv, title) {
+   titleDiv.innerText = $(title).text();
+}
+
+function artist(artistDiv, artist) {
+   artistDiv.innerText = $(artist).text();
+}
+
+function album(albumDiv, album) {
+   albumDiv.innerText = $(album).text();
+}
+
+function duration(durationDiv, duration) {
+   durationDiv.innerText = $(duration).text();
+}
+
+function modal() {
+   // Create a container to hold the dynamically generated divs
+   const songContainer = document.createElement("div");
+   for (const playlist in data.playlists) {
+      //Create a new div for each song
+      const songDiv = document.createElement("div");
+
+      // Set the class for the new div
+      songDiv.className = "song";
+
+      //Populate song cover img
+      var songCoverImg = document.getElementById("coverArt");
+      var img = data.playlists[playlist].playlist_art;
+      playListImg(playListCoverImg, img);
+
+      //Populate song name
+      var songName = document.getElementById("playName");
+      var title = data.playlists[playlist].playlist_name;
       playListName(playListName, name);
 
       //Populate playlist creator
@@ -60,11 +135,8 @@ function home() {
       playlistContainer.appendChild(playlistDiv);
    }
    // Append the container to the body
-   document.body.appendChild(playlistContainer);
-}
-/******************* MODAL *******************/
-function modal() {
-
+   const targetParent = document.getElementById("playlistCards");
+   targetParent.appendChild(playlistContainer);
 }
 
 /******************* MAIN ********************/
